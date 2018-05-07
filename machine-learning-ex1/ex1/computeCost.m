5670da0a962cd9bb1b3a77d4dc1faa9d9f90fae9
Thiloshon <thiloshon@gmail.com>
2018-05-07 22:21:57 +0530

    Week 01 Solution

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -6,17 +6,24 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
+% === === === === === === === = YOUR CODE HERE === === === === === === === =
+% Instructions : Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+% === === === === === === === === === === === === === === === === === === === === === === === === =
 
 
+% Formula is, 1/2m sigma((h0(x) - y)^2)
+% Using h0(x) = theta' x = theta0 + theta1 x,
 
 
+% if h0(x) = theta0 + theta1 x,
+%     J = (1 / (2 * m)) * sum( (theta( 1, 1) + (theta( 2, 1) * X( :, 2)) - y).^2 );
 
-% =========================================================================
+% if h0(x) = theta' x,
+J = (1 / (2 * m)) * sum( ((X * theta) - y).^2 ); % Here theta is not transposed as to comply dot product rules.
+
 
 end
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -8,26 +8,38 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
+    
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
     % ============================================================
-
-    % Save the cost J in every iteration    
+    
+    
+    % Formula is, theta(j) = theta(j) - alpha/m sigma[(h0(x) - y) x(j)]
+    
+    %     For simultanoeus update, store values before updating theta
+    theta1 = theta(1);
+    theta2 = theta(2);
+    h0 = (X * theta - y);
+    
+    % theta(1) = theta1 - ((alpha / m) * sum((X * theta - y)' * X( :, 1)));
+    % Since X( :, 1) is all zeros from intercept term, product is always 0. So we can omit it
+    
+    theta(1) = theta1 - ((alpha / m) * sum(h0));
+    theta(2) = theta2 - ((alpha / m) * sum(h0' * X( :, 2)));
+    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
-
+    
 end
 
 end
+
+% ===================== POSSIBLE SOLUTION 02 =======================================
+
+%     delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2)));
+%     theta = (theta' - (alpha * delta))';
